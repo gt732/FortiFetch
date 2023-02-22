5d59195550292bd82c616c4a75b6e2884fa6b118
GT <geuryt@yahoo.com>
2023-02-22 15:55:43 -0500

    restructured the directory, created first commands for the user

---
+++ app/main.py
@@ -0,0 +1,34 @@
+"""
+This Main module is the entry point of the application. It contains a list of
+commands which can be run from the command line.
+"""
+
+
+# import modules
+import typer
+from rich import print
+from fortifetch.fortifetch import FortiFetch
+
+
+app = typer.Typer()
+
+
+@app.command("create-database")
+def create_sql_database():
+    print("Creating database: FortiFetch.db")
+    FortiFetch.create_sql_database()
+
+
+@app.command("update-all-devices")
+def update_all_devices():
+    print("Updating devices in database")
+    FortiFetch.update_all_devices()
+
+
+@app.command()
+def delete():
+    print("Deleting user: Hiro Hamada")
+
+
+if __name__ == "__main__":
+    app()
